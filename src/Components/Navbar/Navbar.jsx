160879d1de183ff8c52371c0d8a1eeee5fdf6b3b
MOHAMMAD KHUBAIB SARKER <mohammad@kali.kali>
2024-03-28 02:52:43 +0600

    create navbar, and news page

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,4 +1,24 @@
+import { Link, NavLink } from "react-router-dom";
+import logo from "../../assets/images/logo.png";
+
 const Navbar = () => {
+    const navLink = (
+        <>
+            <li>
+                <NavLink to={"/"}>NEWS</NavLink>{" "}
+            </li>
+            <li>
+                <NavLink to={"/destination"}>DESTINATION</NavLink>
+            </li>
+            <li>
+                <NavLink to={"/blog"}>BLOG</NavLink>
+            </li>
+            <li>
+                <NavLink to={"/contact"}>CONTACT</NavLink>
+            </li>
+        </>
+    );
+
     return (
         <div>
             <div className="navbar bg-base-100">
@@ -28,52 +48,20 @@ const Navbar = () => {
                             tabIndex={0}
                             className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52"
                         >
-                            <li>
-                                <a>Item 1</a>
-                            </li>
-                            <li>
-                                <a>Parent</a>
-                                <ul className="p-2">
-                                    <li>
-                                        <a>Submenu 1</a>
-                                    </li>
-                                    <li>
-                                        <a>Submenu 2</a>
-                                    </li>
-                                </ul>
-                            </li>
-                            <li>
-                                <a>Item 3</a>
-                            </li>
+                            {navLink}
                         </ul>
                     </div>
-                    <a className="btn btn-ghost text-xl">daisyUI</a>
+
+                    <img className="btn btn-ghost text-xl" src={logo} alt="" />
                 </div>
-                <div className="navbar-center hidden lg:flex">
-                    <ul className="menu menu-horizontal px-1">
-                        <li>
-                            <a>Item 1</a>
-                        </li>
-                        <li>
-                            <details>
-                                <summary>Parent</summary>
-                                <ul className="p-2">
-                                    <li>
-                                        <a>Submenu 1</a>
-                                    </li>
-                                    <li>
-                                        <a>Submenu 2</a>
-                                    </li>
-                                </ul>
-                            </details>
-                        </li>
-                        <li>
-                            <a>Item 3</a>
-                        </li>
-                    </ul>
-                </div>
+
                 <div className="navbar-end">
-                    <a className="btn">Button</a>
+                    <div className=" hidden lg:flex">
+                        <ul className="menu menu-horizontal px-1">{navLink}</ul>
+                    </div>{" "}
+                    <Link to={"/login"} className="btn">
+                        Login
+                    </Link>
                 </div>
             </div>
         </div>
--- src/Routes/Routes.jsx
+++ src/Routes/Routes.jsx
@@ -1,10 +1,31 @@
 import { createBrowserRouter } from "react-router-dom";
 import Root from "../Components/Root/Root";
+import News from "../Pages/News/News";
+import Error from "../Pages/Error/Error";
 
 const router = createBrowserRouter([
     {
         path: "/",
         element: <Root></Root>,
+        errorElement: <Error></Error>,
+        children: [
+            {
+                path: "/",
+                element: <News></News>,
+            },
+            {
+                path: "/destination",
+                element: <News></News>,
+            },
+            {
+                path: "/blog",
+                element: <News></News>,
+            },
+            {
+                path: "/contact",
+                element: <News></News>,
+            },
+        ],
     },
 ]);
 
